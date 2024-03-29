881ca12b0b85f63303f8b2b451438ac63fbfc4af
Jérémy Remy <dev.kyoma@gmail.com>
2024-03-29 12:10:24 +0100

    FILTRE DATE (again) fonctionnel + ajout colonne inscription.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use App\Entity\Etat;
 use App\Entity\Sortie;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,87 +26,89 @@ class HomeController extends AbstractController
         $this->security = $security;
     }
 
+    #[Route('/annuler-sortie/{id}', name: 'app_annuler_sortie', methods: ['POST'])]
+    public function annulerSortie(Request $request, Sortie $sortie): Response
+    {
+        // Vérifier si la requête est une requête AJAX
+        if (!$request->isXmlHttpRequest()) {
+            // Rediriger vers la page d'accueil ou afficher un message d'erreur
+            return $this->redirectToRoute('app_home');
+        }
+
+        // Récupérer l'état "Annulée"
+        $etatAnnulee = $this->entityManager->getRepository(Etat::class)->findOneBy(['libelle' => 'Annulée']);
+
+        // Mettre à jour l'état de la sortie
+        $sortie->setEtat($etatAnnulee);
+        $this->entityManager->flush();
+
+        // Retourner une réponse JSON indiquant le succès de l'opération
+        return new JsonResponse(['success' => true]);
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(Request $request, UserInterface $user): Response
     {
-        // Récupérer le campus de l'utilisateur connecté
-        $selectedCampusNom = $user->getCampus();
-
-        $selectedCampusNom = $user ? $user->getCampus() : null;
-
-        $selectedCampusNom = $request->query->get('campus');
-
+        // Je récupère le campus de l'utilisateur connecté
         $selectedCampus = null;
-        if ($selectedCampusNom) {
-            $selectedCampus = $this->entityManager->getRepository(Campus::class)->findOneBy(['nom' => $selectedCampusNom]);
-        }
-
-        if (!$this->security->isGranted('ROLE_USER')) {
-            // Redirige vers la page de connexion
-            return new RedirectResponse($this->generateUrl('app_login'));
-        }
-
+        $selectedCampusNom = $request->query->get('campus');
+        $nomRecherche = $request->query->get('nom');
+        $date1 = $request->query->get('date1');
+        $date2 = $request->query->get('date2');
+        $estOrganisateur = $request->query->get('organisateur');
+        $estTerminees = $request->query->get('terminees');
         $campuses = $this->entityManager->getRepository(Campus::class)->findAll();
         $isAdmin = $this->security->isGranted('ROLE_ADMIN');
-        $sorties = [];
 
-        $nomRecherche = $request->query->get('nom'); // Récupérer le nom de recherche depuis la requête
-        $dateDebut = $request->query->get('date_debut'); // Récupérer la date de début depuis la requête
-        $dateFin = $request->query->get('date_fin'); // Récupérer la date de fin depuis la requête
-        $estOrganisateur = $request->query->get('organisateur'); // Récupérer la valeur du filtre organisateur
+        $qb = $this->entityManager->getRepository(Sortie::class)->createQueryBuilder('s');
 
-        $estTerminees = $request->query->get('terminees'); // Récupérer la valeur du filtre sorties terminées
+        if ($selectedCampusNom) {
+            $selectedCampus = $this->entityManager->getRepository(Campus::class)->findOneBy(['nom' => $selectedCampusNom]);
+            $qb->andWhere('s.place = :selectedCampus')
+                ->setParameter('selectedCampus', $selectedCampus);
+        }
 
-        if ($selectedCampus) {
-            if ($nomRecherche) {
-                // Si un nom de recherche est spécifié, filtrer les sorties par nom et campus
-                $sorties = $this->entityManager->getRepository(Sortie::class)
-                    ->createQueryBuilder('s')
-                    ->where('s.place = :selectedCampus')
-                    ->andWhere('s.nom LIKE :nomRecherche')
-                    ->setParameter('selectedCampus', $selectedCampus)
-                    ->setParameter('nomRecherche', '%' . $nomRecherche . '%')
-                    ->getQuery()
-                    ->getResult();
-            } else {
-                // Sinon, récupérer toutes les sorties pour le campus sélectionné
-                $sorties = $this->entityManager->getRepository(Sortie::class)->findBy(['place' => $selectedCampus]);
-            }
-        } else {
-            if ($nomRecherche) {
-                // Si un nom de recherche est spécifié, filtrer toutes les sorties par nom
-                $sorties = $this->entityManager->getRepository(Sortie::class)
-                    ->createQueryBuilder('s')
-                    ->where('s.nom LIKE :nomRecherche')
-                    ->setParameter('nomRecherche', '%' . $nomRecherche . '%')
-                    ->getQuery()
-                    ->getResult();
-            } else {
-                // Sinon, récupérer toutes les sorties
-                $sorties = $this->entityManager->getRepository(Sortie::class)->findAll();
-            }
+        if ($nomRecherche) {
+            $qb->andWhere('s.nom LIKE :nomRecherche')
+                ->setParameter('nomRecherche', '%' . $nomRecherche . '%');
+        }
+
+        if ($date1 && $date2) {
+            // Convertir les chaînes de date en objets DateTime
+            $date1Obj = new \DateTime($date1);
+            $date2Obj = new \DateTime($date2);
+
+            // Ajouter un jour à la date de fin pour inclure les sorties du jour spécifié
+            $date2Obj->modify('+1 day');
+
+            $qb->andWhere('s.dateHeureDebut BETWEEN :date1 AND :date2')
+                ->setParameter('date1', $date1Obj)
+                ->setParameter('date2', $date2Obj);
         }
 
         if ($estOrganisateur) {
-            // Récupérer les sorties où l'utilisateur connecté est l'organisateur
-            $sorties = $this->entityManager->getRepository(Sortie::class)->findBy(['user' => $user]);
-
+            $qb->andWhere('s.user = :user')
+                ->setParameter('user', $user);
         }
 
         if ($estTerminees) {
-            // Récupérer l'objet Etat correspondant à "Terminée"
+            // Je récupère l'objet Etat correspondant à "Terminée"
             $etatTerminee = $this->entityManager->getRepository(Etat::class)->findOneBy(['libelle' => 'Terminée']);
 
             if ($etatTerminee) {
-                // Si l'objet Etat est trouvé, récupérez les sorties avec cet état
-                $sorties = $this->entityManager->getRepository(Sortie::class)->findBy(['etat' => $etatTerminee]);
+                // Si l'objet Etat est trouvé, je récupère les sorties avec cet état
+                $qb->andWhere('s.etat = :etatTerminee')
+                    ->setParameter('etatTerminee', $etatTerminee);
             } else {
                 // Gérer le cas où l'état "Terminée" n'est pas trouvé
-                // Peut-être envoyer un message d'erreur ou une liste vide de sorties
                 $sorties = [];
             }
         }
 
+        $sorties = $qb->getQuery()->getResult();
+
+        $date1 = null;
+        $date2 = null;
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
@@ -114,6 +117,8 @@ class HomeController extends AbstractController
             'sorties' => $sorties,
             'selectedCampus' => $selectedCampus, // Transmettre le campus sélectionné au template
             'user' => $user, // Transmettre l'utilisateur connecté au template
+            'date1' => $date1,
+            'date2' => $date2,
         ]);
     }
 }
