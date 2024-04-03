95c498b5a4a18e177676176eeb6410518a93577a
Jérémy Remy <dev.kyoma@gmail.com>
2024-04-03 16:42:09 +0200

    Ajout campus fonctionnel à partir de panneau d'admin.
    TODO : Supprimer et Modifier
    Vérifier que admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,13 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Campus;
+use App\Form\CampusType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 #[Route('/admin', name: 'admin_')]
 class AdminController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     #[Route('/', name: 'dashboard')]
     public function index(): Response
     {
@@ -22,10 +33,65 @@ class AdminController extends AbstractController
     {
 
     }
-    #[Route('/campus', name: 'campus')]
-    public function manageCampus()
+    #[Route('/campus', name: 'campus', methods: ['GET', 'POST'])]
+    public function manageCampus(Request $request): Response
     {
+        $campus = new Campus(); // Initialisation de la variable campus
+        $form = $this->createForm(CampusType::class, $campus);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Traitement du formulaire et enregistrement du campus
+            $this->entityManager->persist($campus);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Campus ajouté avec succès.');
+
+            return $this->redirectToRoute('admin_campus');
+        }
+
+        // Récupérer tous les campus
+        $campuses = $this->entityManager->getRepository(Campus::class)->findAll();
+
+        return $this->render('admin/manageCampus.html.twig', [
+            'form' => $form->createView(),
+            'campuses' => $campuses, // Passer les campus à la vue
+        ]);
+    }
+
+    #[Route('/campus/add', name: 'campus_add', methods: ['GET', 'POST'])]
+    public function addCampus(Request $request): Response
+    {
+        $campus = new Campus(); // Initialisation du nouvel objet campus
+        $form = $this->createForm(CampusType::class, $campus);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Traitement du formulaire et enregistrement du campus
+            $this->entityManager->persist($campus);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Campus ajouté avec succès.');
+
+            return $this->redirectToRoute('admin_campus');
+        }
+
+        return $this->render('admin/_addCampus.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    #[Route('/campus/edit/{id}', name: 'campus_edit')]
+    public function editCampus(Request $request, Campus $campus): Response
+    {
+        // Votre code pour modifier un campus existant
+    }
+
+    #[Route('/campus/delete/{id}', name: 'campus_delete')]
+    public function deleteCampus(Request $request, Campus $campus): Response
+    {
+        // Votre code pour supprimer un campus existant
     }
     #[Route('/villes', name: 'villes')]
     public function manageVilles()
