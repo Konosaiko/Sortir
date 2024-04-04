3d70a6e0da307617c6a6043b02b17dee521b24a4
Jérémy Remy <dev.kyoma@gmail.com>
2024-04-04 10:30:41 +0200

    Gestion des campus ;
    Ajout, modifier ou supprimer des
    campus.

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Campus;
 use App\Form\CampusType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,9 +17,12 @@ class AdminController extends AbstractController
 {
     private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    private $security;
+
+    public function __construct(EntityManagerInterface $entityManager, Security $security)
     {
         $this->entityManager = $entityManager;
+        $this->security = $security;
     }
     #[Route('/', name: 'dashboard')]
     public function index(): Response
@@ -82,17 +86,42 @@ class AdminController extends AbstractController
     }
 
 
+
     #[Route('/campus/edit/{id}', name: 'campus_edit')]
     public function editCampus(Request $request, Campus $campus): Response
     {
-        // Votre code pour modifier un campus existant
+        // Vérifier si le formulaire de modification a été soumis
+        if ($request->isMethod('POST')) {
+            // Récupérer le nouveau nom du campus à partir des données du formulaire
+            $newName = $request->request->get('newName');
+
+            // Mettre à jour le nom du campus dans la base de données
+            $campus->setNom($newName);
+
+            // Enregistrer les modifications dans la base de données
+            $entityManager = $this->entityManager;
+            $entityManager->flush();
+
+            // Rediriger l'utilisateur vers la page de gestion des campus avec un message de succès
+            $this->addFlash('success', 'Le nom du campus a été modifié avec succès.');
+            return $this->redirectToRoute('admin_campus');
+        }
     }
 
     #[Route('/campus/delete/{id}', name: 'campus_delete')]
     public function deleteCampus(Request $request, Campus $campus): Response
     {
-        // Votre code pour supprimer un campus existant
+        // Supprimez le campus de la base de données
+        $this->entityManager->remove($campus);
+        $this->entityManager->flush();
+
+        // Ajoutez un message flash pour confirmer la suppression
+        $this->addFlash('success', 'Le campus a été supprimé avec succès.');
+
+        // Redirigez l'utilisateur vers la page des campus
+        return $this->redirectToRoute('admin_campus');
     }
+
     #[Route('/villes', name: 'villes')]
     public function manageVilles()
     {
